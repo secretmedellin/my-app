f69ba4ce8fa68e1b26574d68dd3cd493d79178ab
secretmedellin <dordaz29@hotmail.com>
2023-02-11 17:07:55 -0800

    Test netlify deployment

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go live On Netlify
         </a>
       </header>
     </div>
